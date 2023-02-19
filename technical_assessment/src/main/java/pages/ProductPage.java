d79f7ce7a7e15e3362ca86348aa4ed0feca92d40
Shweta Bansal <shweta.bansal@impetus.com>
2023-02-19 19:50:26 +0530

    infinite error resolved

--- technical_assessment/src/main/java/pages/ProductPage.java
+++ technical_assessment/src/main/java/pages/ProductPage.java
@@ -70,6 +70,7 @@ public class ProductPage {
     	wait.until(ExpectedConditions.presenceOfElementLocated(product_name));
     	String name="";
     	while(name.isEmpty()) {
+
     		name=driver.findElement(product_name).getText();
     	}
 		return name;
@@ -81,6 +82,7 @@ public class ProductPage {
     	wait.until(ExpectedConditions.presenceOfElementLocated(product_price));
     	String price="";
     	while(price.isEmpty()) {
+			driver.navigate().refresh();
     		price=driver.findElement(product_price).getText();
     	}
 		return price;
